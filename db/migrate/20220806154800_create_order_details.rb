73749a1ceba7325d0ed2bd95e47078e06909c2e6
lrodriguez06 <lissette.rodriguezmedina@gmail.com>
2022-08-23 21:50:05 -0400

    Create 20220806154800_create_order_details.rb

---
+++ db/migrate/20220806154800_create_order_details.rb
@@ -0,0 +1,12 @@
+class CreateOrderDetails < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_details do |t|
+      t.references :order, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+      t.integer :cantidad
+      t.decimal :precio
+
+      t.timestamps
+    end
+  end
+end
